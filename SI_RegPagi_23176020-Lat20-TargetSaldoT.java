5db8cab4228f3b252c72a768e10b1fb795b0f751
dzurrotuumi13 <161183181+dzurrotuumi13@users.noreply.github.com>
2024-10-30 07:58:56 +0700

    Update SI_RegPagi_23176020-Lat20-TargetSaldoT.java

--- SI_RegPagi_23176020-Lat20-TargetSaldoT.java
+++ SI_RegPagi_23176020-Lat20-TargetSaldoT.java
@@ -7,6 +7,10 @@ package pkg23176020pbo5;
 /**
  *
  * @author Asus
+ * Nama : Dzurrotu Umi Asyfiya
+ * NIM : 23176020 
+ * Prodi : Sistem Informasi 
+ * Deskripsi Program: Rangkaian kode berikut berisi mengenai Target Saldo Tabungan Bulanan
  */
 public class TargetSaldoT {
   
