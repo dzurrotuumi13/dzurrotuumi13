fb8b7732c52b58ab523b33ce13ec0959a33d2b7c
dzurrotuumi13 <161183181+dzurrotuumi13@users.noreply.github.com>
2024-11-01 16:21:24 +0700

    Update and rename HurufBesarKecil.java to SI_RegPagi_23176020-Lat27-HurufBesarKecil.java

--- SI_RegPagi_23176020-Lat27-HurufBesarKecil.java
+++ SI_RegPagi_23176020-Lat27-HurufBesarKecil.java
@@ -7,6 +7,10 @@ package pkg23176020pbo5;
 /**
  *
  * @author Asus
+ * Nama : Dzurrotu Umi Asyfiya
+ * NIM : 23176020
+ * Prodi : Sistem Informasi
+ * Deskripsi Program: Rangkaian kode berikut berisi mengenai Waktu saat ini
  */
 import java.util.Scanner;
 
